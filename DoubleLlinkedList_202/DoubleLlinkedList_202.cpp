b4acc4ba4536725bddb289df988452a56cabed74
wawanpi <muhaguskurniawan@gmail.com>
2023-05-26 18:38:23 +0700

    step 1.d dan step 1.e

--- DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
+++ DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
@@ -79,8 +79,8 @@ bool Doublelinkedlist::search(int rollNo, Node** previous, Node** current) {
 	*previous = START;//step 1.a
 	*current = START;//step 1.b
 	while (*current != NULL && rollNo != (*current)->noMhs) { //step 1.c
-		*previous = START; //step 1.a
-		*current = START;//step 1.b
+		*previous = START; //step 1.d
+		*current = START;//step 1.e
 	}
 	{
 
