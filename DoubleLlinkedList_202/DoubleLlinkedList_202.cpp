646ddf435daef1a8f8f7aeb1ba03c82ae2989988
wawanpi <muhaguskurniawan@gmail.com>
2023-05-26 11:19:58 +0700

    6

--- DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
+++ DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
@@ -20,6 +20,21 @@ public:
 	bool deleteNode(int rolNo);
 	bool listEmpty();
 	void traverse();
+	void retraverse();
 	void hapus();
 	void searchData();
 };
+
+Doublelinkedlist::Doublelinkedlist() {
+	START =  NULL;
+}
+
+void Doublelinkedlist::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nm;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node()
+}
