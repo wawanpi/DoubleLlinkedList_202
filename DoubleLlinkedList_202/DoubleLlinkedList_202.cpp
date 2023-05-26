ae78965708d955007b341ffc7d0779fe700d56e1
wawanpi <muhaguskurniawan@gmail.com>
2023-05-26 11:12:54 +0700

    3

--- DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
+++ DoubleLlinkedList_202/DoubleLlinkedList_202.cpp
@@ -14,5 +14,12 @@ class Doublelinkedlist {
 private:
 	Node* START;
 public:
-
+	Doublelinkedlist();
+	void addNode();
+	bool search(int rolNO, Node** previous, Node** current);
+	bool deleteNode(int rolNo);
+	bool listEmpty();
+	void traverse();
+	void hapus();
+	void searchData();
 };
